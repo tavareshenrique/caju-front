7b1a81add61c9bde8e7f634c4f7b4908b47d7c67
Henrique Tavares <ihenrits@gmail.com>
2024-10-15 20:52:56 -0300

    refactor: Update Button component styles to allow custom background color and text color

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -1,16 +1,19 @@
 import styled from 'styled-components';
 
-const Button = styled.button`
+const Button = styled.button<{
+	bgcolor?: string;
+	color?: string;
+}>`
 	outline: none;
 	display: flex;
 	align-items: center;
 	border: none;
 	border-radius: 36px;
 	padding: 8px 32px;
-	background-color: #64a98c;
+	background-color: ${(props) => props.bgcolor ?? '#64a98c'};
 	cursor: pointer;
 	height: 56px;
-	color: #fff;
+	color: ${(props) => props.color ?? '#fff'};
 	box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 	font-size: 16px;
 	font-weight: 600;
