255b73eb83df62f5b1a13e30d834985bb43b4951
Henrique Tavares <ihenrits@gmail.com>
2024-10-15 16:26:50 -0300

    feat: add React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import Router from '@/router';
 
 import { Header } from './components/Header';
+import { ReactQueryProvider } from './libs/ReactQueryProvider';
 
 function App() {
-  return (
-    <>
-      <Header>
-        <h1>Caju Front Teste</h1>
-      </Header>
-      <Router />
-    </>
-  );
+	return (
+		<ReactQueryProvider>
+			<Header>
+				<h1>Caju Front Teste</h1>
+			</Header>
+
+			<Router />
+		</ReactQueryProvider>
+	);
 }
 
 export default App;
