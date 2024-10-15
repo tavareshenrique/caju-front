e557243b54ff2095193564f7a11267f24799d154
Henrique Tavares <ihenrits@gmail.com>
2024-10-15 16:27:28 -0300

    refactor: Update Dashboard component to fetch and display registration data using React Query

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,39 @@
-import Collumns from './components/Columns';
+import { useQuery } from '@tanstack/react-query';
+
+import api from '@/libs/axios';
+
+import Columns from './components/Columns';
 import { SearchBar } from './components/Searchbar';
 import * as S from './styles';
 
+export type TRegistrationStatus = 'APPROVED' | 'REJECTED' | 'PENDING';
+
+export interface IRegistration {
+	id: string;
+	admissionDate: string;
+	email: string;
+	employeeName: string;
+	status: TRegistrationStatus;
+	cpf: string;
+}
+
 const DashboardPage = () => {
+	const {
+		data: registrationsData,
+		// isLoading,
+		// error,
+	} = useQuery({
+		queryKey: ['registrations'],
+		queryFn: async () => {
+			return (await api.get<IRegistration[]>('/registrations')).data;
+		},
+		// select: (data) => makeOrderDetails(data),
+	});
+
 	return (
 		<S.Container>
 			<SearchBar />
-			<Collumns registrations={[]} />
+			<Columns registrations={registrationsData} />
 		</S.Container>
 	);
 };
