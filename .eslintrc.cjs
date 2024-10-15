44425e5f75d0f487f0ff8c44668d650ef729968a
Henrique Tavares <ihenrits@gmail.com>
2024-10-15 16:26:07 -0300

    chore: Refactor import paths and update code formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,10 @@
 module.exports = {
-  extends: ['eslint-config-henrique/react'],
-  plugins: ['simple-import-sort'],
-  rules: {
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-  },
+	extends: ['eslint-config-henrique/react'],
+	plugins: ['simple-import-sort'],
+	rules: {
+		'simple-import-sort/imports': 'error',
+		'simple-import-sort/exports': 'error',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'error',
+	},
 };
