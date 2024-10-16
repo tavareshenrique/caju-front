9be76b7428638952d8c48dbfffc2df842a5bde78
Henrique Tavares <ihenrits@gmail.com>
2024-10-16 11:15:12 -0300

    refactor: Update TextField component to make onClearIconClick prop optional

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 import { MdClear } from 'react-icons/md';
 
 import { Input, TextFieldContainer, TextFieldContent } from './style';
@@ -9,37 +9,34 @@ type TTextFieldProps = {
 	onClearIconClick?: () => void;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-const TextField = ({
-	label,
-	id,
-	error,
-	value,
-	onClearIconClick,
-	...rest
-}: TTextFieldProps) => {
-	return (
-		<>
-			{label && <label htmlFor={id}>{label}</label>}
+const TextField = forwardRef<HTMLInputElement, TTextFieldProps>(
+	({ label, id, error, value, onClearIconClick, ...rest }, ref) => {
+		return (
+			<>
+				{label && <label htmlFor={id}>{label}</label>}
 
-			<TextFieldContainer>
-				<TextFieldContent>
-					<Input {...rest} value={value} id={id} />
+				<TextFieldContainer>
+					<TextFieldContent>
+						<Input {...rest} id={id} ref={ref} />
 
-					{onClearIconClick && (
-						<button
-							type="button"
-							onClick={onClearIconClick}
-							disabled={!value}
-							aria-label="Limpar campo"
-						>
-							<MdClear size={16} color={value ? '#222222' : '#b7b7b7'} />
-						</button>
-					)}
-				</TextFieldContent>
-				<span style={{ fontSize: 12, color: 'red' }}>{error}</span>
-			</TextFieldContainer>
-		</>
-	);
-};
+						{onClearIconClick && (
+							<button
+								type="button"
+								onClick={onClearIconClick}
+								disabled={!value}
+								aria-label="Limpar campo"
+							>
+								<MdClear size={16} color={value ? '#222222' : '#b7b7b7'} />
+							</button>
+						)}
+					</TextFieldContent>
+					<span style={{ fontSize: 12, color: 'red' }}>{error}</span>
+				</TextFieldContainer>
+			</>
+		);
+	},
+);
+
+TextField.displayName = 'TextField';
 
 export default TextField;
