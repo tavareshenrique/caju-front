b43e4b118893890e8e483259ed17cdd43867498a
Henrique Tavares <ihenrits@gmail.com>
2024-10-16 10:07:55 -0300

    refactor: Update TextField component to make onClearIconClick prop optional

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -6,7 +6,7 @@ import { Input, TextFieldContainer, TextFieldContent } from './style';
 type TTextFieldProps = {
 	label?: string;
 	error?: string;
-	onClearIconClick: () => void;
+	onClearIconClick?: () => void;
 } & InputHTMLAttributes<HTMLInputElement>;
 
 const TextField = ({
@@ -18,22 +18,27 @@ const TextField = ({
 	...rest
 }: TTextFieldProps) => {
 	return (
-		<TextFieldContainer>
+		<>
 			{label && <label htmlFor={id}>{label}</label>}
-			<TextFieldContent>
-				<Input {...rest} value={value} />
 
-				<button
-					type="button"
-					onClick={onClearIconClick}
-					disabled={!value}
-					aria-label="Limpar campo"
-				>
-					<MdClear size={16} color={value ? '#222222' : '#b7b7b7'} />
-				</button>
-			</TextFieldContent>
-			<span style={{ fontSize: 12, color: 'red' }}>{error}</span>
-		</TextFieldContainer>
+			<TextFieldContainer>
+				<TextFieldContent>
+					<Input {...rest} value={value} id={id} />
+
+					{onClearIconClick && (
+						<button
+							type="button"
+							onClick={onClearIconClick}
+							disabled={!value}
+							aria-label="Limpar campo"
+						>
+							<MdClear size={16} color={value ? '#222222' : '#b7b7b7'} />
+						</button>
+					)}
+				</TextFieldContent>
+				<span style={{ fontSize: 12, color: 'red' }}>{error}</span>
+			</TextFieldContainer>
+		</>
 	);
 };
 
