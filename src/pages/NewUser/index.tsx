9730a92a26b4cbad200cf8cb0011781b901e6f4b
Henrique Tavares <ihenrits@gmail.com>
2024-10-16 11:15:20 -0300

    feat: Add new user registration functionality
    This commit adds the necessary code to handle new user registrations. It includes the creation of a new registration schema, form validation using zod, and the implementation of the createRegistration function. The form fields for name, email, CPF, and admission date are also added to the NewUserPage component.

--- src/pages/NewUser/index.tsx
+++ src/pages/NewUser/index.tsx
@@ -1,20 +1,64 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Helmet } from 'react-helmet';
+import { Controller, useForm } from 'react-hook-form';
 import { HiOutlineArrowLeft } from 'react-icons/hi';
 import { useHistory } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
+import { z } from 'zod';
 
 import Button from '@/components/Buttons';
 import { IconButton } from '@/components/Buttons/IconButton';
 import TextField from '@/components/TextField';
+import { cpf } from '@/helpers/cpf';
+import { createRegistration } from '@/repositories/create-registration';
 import routes from '@/router/routes';
 
 import * as S from './styles';
 
+const newRegistrationSchema = z.object({
+	id: z.string().uuid(),
+	admissionDate: z.string(),
+	email: z.string().email('O email deve ser válido'),
+	status: z.string().default('PENDING'),
+	employeeName: z
+		.string()
+		.min(2, { message: 'O nome deve ter no mínimo 2 caracteres' })
+		.regex(/^[^\d]/, 'O nome não pode começar com um número'),
+	cpf: z.string().refine(cpf.validator, {
+		message: 'CPF inválido',
+	}),
+});
+
+export type TNewRegistrationSchema = z.infer<typeof newRegistrationSchema>;
+
 const NewUserPage = () => {
+	const {
+		handleSubmit,
+		control,
+		// formState: { errors },
+	} = useForm<TNewRegistrationSchema>({
+		resolver: zodResolver(newRegistrationSchema),
+		defaultValues: {
+			id: uuid(),
+			admissionDate: '',
+			email: '',
+			status: 'PENDING',
+			employeeName: '',
+			cpf: '',
+		},
+	});
+
 	const history = useHistory();
 
 	const goToHome = () => {
 		history.push(routes.dashboard);
 	};
+
+	const onSubmit = handleSubmit(async (data) => {
+		await createRegistration(data);
+
+		goToHome();
+	});
 
 	return (
 		<>
@@ -26,16 +70,49 @@ const NewUserPage = () => {
 				/>
 			</Helmet>
 
-			<S.Container>
+			<S.Container onSubmit={onSubmit}>
 				<S.Card>
 					<IconButton onClick={() => goToHome()} aria-label="back">
 						<HiOutlineArrowLeft size={24} />
 					</IconButton>
-					<TextField placeholder="Nome" label="Nome" />
-					<TextField placeholder="Email" label="Email" type="email" />
-					<TextField placeholder="CPF" label="CPF" />
-					<TextField label="Data de admissão" type="date" />
-					<Button onClick={() => {}}>Cadastrar</Button>
+
+					<Controller
+						name="employeeName"
+						control={control}
+						render={({ field }) => (
+							<TextField {...field} placeholder="Nome" label="Nome" />
+						)}
+					/>
+
+					<Controller
+						name="email"
+						control={control}
+						render={({ field }) => (
+							<TextField
+								{...field}
+								placeholder="Email"
+								label="Email"
+								type="email"
+							/>
+						)}
+					/>
+
+					<Controller
+						name="cpf"
+						control={control}
+						render={({ field }) => (
+							<TextField {...field} placeholder="CPF" label="CPF" />
+						)}
+					/>
+
+					<Controller
+						name="admissionDate"
+						control={control}
+						render={({ field }) => (
+							<TextField {...field} label="Data de admissão" type="date" />
+						)}
+					/>
+					<Button type="submit">Cadastrar</Button>
 				</S.Card>
 			</S.Container>
 		</>
