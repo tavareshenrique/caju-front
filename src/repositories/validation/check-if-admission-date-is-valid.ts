f02935d171c683ab30150449736529ebc8d2295f
Henrique Tavares <ihenrits@gmail.com>
2024-10-18 15:03:35 -0300

    refactor: Update checkIfAdmissionDateIsValid to remove console logs and improve error message

--- src/repositories/validation/check-if-admission-date-is-valid.ts
+++ src/repositories/validation/check-if-admission-date-is-valid.ts
@@ -3,9 +3,6 @@ import { ValidationError } from '../errors/validation-error';
 async function checkIfAdmissionDateIsValid(admissionDate: string) {
 	const registerDate = new Date(admissionDate);
 
-	console.log(registerDate);
-	console.log('check', registerDate > new Date());
-
 	if (registerDate > new Date()) {
 		throw new ValidationError(
 			'A data de admissão não pode ser uma data futura',
