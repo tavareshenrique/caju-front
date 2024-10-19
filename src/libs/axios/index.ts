d4f4025071575c28d9aba2106e574129e6fbc936
Henrique Tavares <ihenrits@gmail.com>
2024-10-19 16:39:23 -0300

    fix: Update axios base URL for API requests

--- src/libs/axios/index.ts
+++ src/libs/axios/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL =
 	process.env.NODE_ENV === 'production'
-		? 'https://caju-server.onrender.com/registrations'
+		? 'https://caju-server.onrender.com'
 		: 'http://localhost:3000';
 
 const api = axios.create({
