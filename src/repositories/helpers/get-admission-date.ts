11457ab00ab0a2434075262f031fe63d0415121e
Henrique Tavares <ihenrits@gmail.com>
2024-10-19 12:35:31 -0300

    fix: Update getAdmissionDate to include time in admission date
    The getAdmissionDate function in the src/repositories/helpers/get-admission-date.ts file has been updated to include the time in the admission date. This change ensures that the formatted admission date includes the time as well, by appending 'T00:00:00' to the input date.
    
    Refactor the getAdmissionDate function to include time in the admission date

--- src/repositories/helpers/get-admission-date.ts
+++ src/repositories/helpers/get-admission-date.ts
@@ -1,6 +1,6 @@
 function getAdmissionDate(admissionDate: string) {
 	const formattedAdmissionDate = Intl.DateTimeFormat('pt-BR').format(
-		new Date(admissionDate),
+		new Date(admissionDate + 'T00:00:00'),
 	);
 
 	return formattedAdmissionDate;
