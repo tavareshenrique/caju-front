151c269881477316649aebb6187c48063651435f
Henrique Tavares <ihenrits@gmail.com>
2024-10-19 16:37:07 -0300

    refactor: Update axios base URL for API requests

--- src/libs/axios/index.ts
+++ src/libs/axios/index.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const baseURL =
+	process.env.NODE_ENV === 'production'
+		? 'https://caju-server.onrender.com/registrations'
+		: 'http://localhost:3000';
+
 const api = axios.create({
-	baseURL: 'http://localhost:3000',
+	baseURL,
 });
 
 export default api;
