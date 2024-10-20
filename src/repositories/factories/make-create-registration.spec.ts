51e10bf89908a8ab6aaa52f3fdfb785a2f4355e0
Henrique Tavares <ihenrits@gmail.com>
2024-10-20 11:29:58 -0300

    test: Update make-create-registration.spec.ts to mock dependencies for testing

--- src/repositories/factories/make-create-registration.spec.ts
+++ src/repositories/factories/make-create-registration.spec.ts
@@ -1,9 +1,14 @@
 import { faker } from '@faker-js/faker/locale/pt_BR';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 import { TNewRegistrationSchema } from '@/pages/NewUser/hooks/useNewUserPage';
 
 import { makeCreateRegistration } from './make-create-registration';
+
+vi.mock('@/libs/axios');
+vi.mock('./fetch-registrations');
+vi.mock('../validation/check-cpf-is-already-in-use');
+vi.mock('../validation/check-email-is-already-in-use');
 
 describe('makeCreateRegistration', () => {
 	it('should return a new registration', async () => {
